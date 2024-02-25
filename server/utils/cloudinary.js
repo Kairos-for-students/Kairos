14881b11a3b3e714c4cdfcfa89893d6ac7347c80
Dev Bulchandani <devbulchandani8@gmail.com>
2024-02-25 10:32:40 +0530

    cloudinary test 2

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -3,7 +3,7 @@ import { v2 as cloudinary } from 'cloudinary';
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
-    api_secret: process.env.SECRET_API_KEY,
+    api_secret: process.env.API_SECRET_KEY,
 });
 
 export default cloudinary;
