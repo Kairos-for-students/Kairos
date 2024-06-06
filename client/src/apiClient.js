74f65d0cc65811b6fbc5af5a2092f4121ab06964
Dev Bulchandani <devbulchandani8@gmail.com>
2024-06-06 15:19:33 +0530

    cicd pipeline improve

--- client/src/apiClient.js
+++ client/src/apiClient.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 
 export const register = async (formdata) => {
     // console.log("Calling register function with formdata:", formdata);
